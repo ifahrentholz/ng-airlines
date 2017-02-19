b3aba07145267c8d5de14c717d10e24d1c785e81
Ingo Fahrentholz <ingof.work@gmail.com>
2017-02-19 18:03:54 +0100

    NG2 Airlines Tut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {PassengerDashboardModule} from "./passenger-dashboard/passenger-dashboard.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    PassengerDashboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
